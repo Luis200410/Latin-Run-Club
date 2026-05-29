e88d86c7d401713138867102cd7ff7642db05087
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-05-29 15:11:43 -0400

    changed the animation to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import MainIndex from "../components/MainIndex";
 import Excuse from "../components/Excuse";
 import EventsContainer from "../components/EventsContainer";
 import BrandContainer from "../components/BrandContainer";
+import Gallery from "./Gallery";
 import '../style/main-index.css';
 import '../style/excuse.css';
 import '../style/brands.css';
@@ -9,6 +10,7 @@ import '../style/brands.css';
 export default function Home() {
     return (
         <>
+            <Gallery />
             <MainIndex />
             <Excuse />
             <EventsContainer />
